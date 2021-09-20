8c36ffca44391860c55e15e52b0926fbed8644a0
dhatchi711 <kgdhatchi@gmail.com>
2021-09-19 22:45:52 -0700

    added spacing

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     throw error;
 });
 
-app.use((error, req,res, next) => {
+app.use((error, req, res, next) => {
     if(res.headerSent){
         return next(error);
     }
