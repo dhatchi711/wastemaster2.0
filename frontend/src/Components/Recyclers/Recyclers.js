e0637da4c3a2071071a1b12be9441e6554e34fa5
dhatchi711 <kgdhatchi@gmail.com>
2021-09-20 14:05:57 -0700

    added http hook for getUsers

--- frontend/src/Components/Recyclers/Recyclers.js
+++ frontend/src/Components/Recyclers/Recyclers.js
@@ -2,40 +2,28 @@ import React, {useEffect, useState} from 'react'
 import RecyclingList from './RecyclingList'
 import ErrorModal from '../UIElements/ErrorModal';
 import LoadingSpinner from '../UIElements/LoadingSpinner';
+import { useHttpClient } from '../hooks/http-hook';
 
 const Recyclers = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
     const [loadedUsers, setLoadedUsers] = useState();
   
     useEffect(() => {
-      const sendRequest = async () => {
-        setIsLoading(true);
+      const fetchUsers = async () => {
         try {
-          const response = await fetch('http://localhost:5000/api/users');
-  
-          const responseData = await response.json();
-  
-          if (!response.ok) {
-            throw new Error(responseData.message);
-          }
+          const responseData = await sendRequest(
+            'http://localhost:5000/api/users'
+          );
   
           setLoadedUsers(responseData.users);
-        } catch (err) {
-          setError(err.message);
-        }
-        setIsLoading(false);
+        } catch (err) {}
       };
-      sendRequest();
-    }, []);
-  
-    const errorHandler = () => {
-      setError(null);
-    };
+      fetchUsers();
+    }, [sendRequest]);
   
     return (
       <React.Fragment>
-        <ErrorModal error={error} onClear={errorHandler} />
+        <ErrorModal error={error} onClear={clearError} />
         {isLoading && (
           <div className="center">
             <LoadingSpinner />
@@ -45,5 +33,5 @@ const Recyclers = () => {
       </React.Fragment>
     );
   };
-  
+    
 export default Recyclers;
