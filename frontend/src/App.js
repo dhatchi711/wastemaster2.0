aed7c30bd103505dd2f7b4cc33b736ffc09f70cb
dhatchi711 <kgdhatchi@gmail.com>
2021-09-19 21:06:14 -0700

    completed frontend including auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
@@ -12,35 +12,68 @@ import MainNavigation from './Components/Navigation/MainNavigation';
 import UserPlaces from './Components/UserPages/UserPlaces';
 import NewPlace from './Components/UserPages/NewPlace';
 import UpdatePlace from './Components/UserPages/UpdatePlace';
+import Auth from './Components/Recyclers/pages/Auth';
+import { AuthContext } from './Components/context/auth-context';
 
-function App() {
-  return (
-    <Router>
-      <MainNavigation/>
-      <main>
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
       <Switch>
-          <Route path='/' exact component={Recyclers} />
-          <Route path='/about' component={About}/>
-          <Route path='/recyclers'>
-            <Recyclers/>
-          </Route>
-          <Route path='/:userId/items' exact>
-            <UserPlaces/>
-          </Route>
-          <Route path="/recycling/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/recycling/:recyclingId">
-            <UpdatePlace />
-          </Route>
-          <Route path='/recyclingnearme'>
-            <RecyclingNearMe/>
-          </Route>
-          <Redirect to='/'/>
+        <Route path="/" exact>
+          <Recyclers />
+        </Route>
+        <Route path='/:userId/recycle' exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/recycling/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/recycling/:recyclingId">
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
       </Switch>
-      </main>
-    </Router>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Recyclers />
+        </Route>
+        <Route path="/:userId/items" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
   );
+
 }
 
 export default App;
--- frontend/src/Components/Navigation/NavLinks.js
+++ frontend/src/Components/Navigation/NavLinks.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {NavLink} from 'react-router-dom';
-
+import { AuthContext } from '../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks = props => {
+    const auth = useContext(AuthContext);
+
     return <ul className='nav-links'>
         <li>
             <NavLink to='/recyclers'>Recyclers</NavLink>
         </li>
+        {auth.isLoggedIn && (
         <li>
             <NavLink to='/u1/recycle'>My Recycling</NavLink>
-        </li>
+        </li>)}
+        {auth.isLoggedIn && (
         <li>
             <NavLink to='/recycling/new'>Add Item</NavLink>
-        </li>
+        </li>)}
+        {!auth.isLoggedIn && (
         <li>
             <NavLink to='/auth'>LogIn/SignUp</NavLink>
-        </li>
+        </li>)}
+        {auth.isLoggedIn && (
+        <li><button onClick={auth.logout}>LOG OUT</button></li>)}
     </ul>
 }
 
