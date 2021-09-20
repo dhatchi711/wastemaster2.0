3cb3c28ac245e25895100f6c6dd49965c1a17615
dhatchi711 <kgdhatchi@gmail.com>
2021-09-20 13:00:41 -0700

    added get users and login routes

--- frontend/src/Components/Recyclers/Recyclers.js
+++ frontend/src/Components/Recyclers/Recyclers.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import RecyclingList from './RecyclingList'
+import ErrorModal from '../UIElements/ErrorModal';
+import LoadingSpinner from '../UIElements/LoadingSpinner';
 
 const Recyclers = () => {
-    const RECYCLERS = [
-        {
-            id: 'u1',
-            name: 'Deepak',
-            image: 'https://www.gtlaw.com/-/media/images/team/p/person-philip-i/34485largepng.png',
-            recycleCount: 3
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [loadedUsers, setLoadedUsers] = useState();
+  
+    useEffect(() => {
+      const sendRequest = async () => {
+        setIsLoading(true);
+        try {
+          const response = await fetch('http://localhost:5000/api/users');
+  
+          const responseData = await response.json();
+  
+          if (!response.ok) {
+            throw new Error(responseData.message);
+          }
+  
+          setLoadedUsers(responseData.users);
+        } catch (err) {
+          setError(err.message);
         }
-    ];
-    return <RecyclingList items={RECYCLERS}/>;
-};
-
+        setIsLoading(false);
+      };
+      sendRequest();
+    }, []);
+  
+    const errorHandler = () => {
+      setError(null);
+    };
+  
+    return (
+      <React.Fragment>
+        <ErrorModal error={error} onClear={errorHandler} />
+        {isLoading && (
+          <div className="center">
+            <LoadingSpinner />
+          </div>
+        )}
+        {!isLoading && loadedUsers && <RecyclingList items={loadedUsers} />}
+      </React.Fragment>
+    );
+  };
+  
 export default Recyclers;
